86d5a2767edfd180e71ab1b76c0a9405afee44f1
Severyn <severynson.kurach@gmail.com>
2022-12-22 13:03:41 +0200

    Section 2 - 1/2 DONE

--- My/App.js
+++ My/App.js
@@ -1,6 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
-import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import {
+  Button,
+  FlatList,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 
 export default function App() {
   const [enteredGoalText, setEnteredGoalText] = useState("");
@@ -13,55 +20,40 @@ export default function App() {
   function addGoalHandler() {
     setCourseGoals((previousExistingGoals) => [
       ...previousExistingGoals,
-      enteredGoalText,
+      { text: enteredGoalText, key: Math.random().toString() },
     ]);
+    setEnteredGoalText("");
   }
 
   return (
-    // <View style={styles.container}>
-    //   <View>
-    //     <Text style={styles.text}>Another piece of text!</Text>
-    //   </View>
-
-    //   <Text style={styles.dummyText}>Hello World!</Text>
-    //   <Button title="Tap me!" />
-    // </View>
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
         <TextInput
           style={styles.textInput}
           placeholder="Your course goal!"
           onChangeText={goalInputHandler}
+          value={enteredGoalText}
         />
         <Button title="Add Goal" onPress={addGoalHandler} />
       </View>
       <View style={styles.goalsContainer}>
-        <Text>List of goals...</Text>
-        {courseGoals.map((goal) => (
-          <View key={goal} style={styles.goalItem}>
-            <Text style={styles.goalText}>{goal}</Text>
-          </View>
-        ))}
+        {!courseGoals.length && <Text>List of goals is empty</Text>}
+        <FlatList
+          data={courseGoals}
+          renderItem={({ item: { text, id } }) => (
+            <View style={styles.goalItem}>
+              <Text style={styles.goalText}>{text}</Text>
+            </View>
+          )}
+          keyExtractor={({ key }) => key}
+          alwaysBounceVertical={false}
+        />
       </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  // container: {
-  //   flex: 1,
-  //   backgroundColor: "#fff",
-  //   alignItems: "center",
-  //   justifyContent: "center",
-  // },
-  // dummyText: {
-  //   margin: 16,
-  //   padding: 6,
-  //   borderWidth: 1,
-  //   borderRadius: 5,
-  //   borderColor: "orange",
-  // },
-  // text: { fontSize: 21, fontWeight: "400" },
   appContainer: {
     flex: 1,
     paddingTop: 50,
@@ -72,6 +64,8 @@ const styles = StyleSheet.create({
     justifyContent: "space-between",
     alignItems: "center",
     marginBottom: 24,
+    paddingTop: 12,
+    paddingBottom: 24,
     borderBottomWidth: 1,
     borderBottomColor: "#cccccc",
   },
