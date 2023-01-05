120bff2eb4422238ea8edba6c9f51532986f9f68
Severyn <severynson.kurach@gmail.com>
2023-01-05 14:33:02 +0200

    Section 6 app development in progres... first 0.5 hours done+++

--- Section 6/App.js
+++ Section 6/App.js
@@ -1,12 +1,21 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
 import CategoriesScreen from "./screens/CategoriesScreen";
+
+const Stack = createNativeStackNavigator();
+const { Navigator } = Stack;
 
 export default function App() {
   return (
     <>
       <StatusBar style="light" />
-      <CategoriesScreen />
+      <NavigationContainer>
+        <Navigator></Navigator>
+        <CategoriesScreen />
+      </NavigationContainer>
     </>
   );
 }
