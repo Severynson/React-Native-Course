b7a492b1d20b736730eaf7f8e8333a28aed74c9d
Severyn <severynson.kurach@gmail.com>
2023-01-12 12:26:21 +0200

    Section 8 DONE

--- Section-7/components/ExpenseOutput/ExpensesOutput.js
+++ Section-7/components/ExpenseOutput/ExpensesOutput.js
@@ -1,4 +1,4 @@
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import { GlobalStyles } from "../../constants/styles";
 import ExpensesList from "./ExpensesList";
 import ExpensesSummary from "./ExpensesSummary";
@@ -6,11 +6,16 @@ import ExpensesSummary from "./ExpensesSummary";
 export default function ExpensesOutput({
   expenses,
   expensesPeriod,
+  fallbackText,
 }) {
+  let content;
+  if (expenses.length) content = <ExpensesList {...{ expenses }} />;
+  else content = <Text style={styles.infoText}>{fallbackText}</Text>;
+
   return (
     <View style={styles.container}>
       <ExpensesSummary {...{ expenses }} periodName={expensesPeriod} />
-      <ExpensesList {...{ expenses }} />
+      {content}
     </View>
   );
 }
@@ -22,5 +27,11 @@ const styles = StyleSheet.create({
     paddingTop: 24,
     paddingBottom: 0,
     backgroundColor: GlobalStyles.colors.primary700,
+  },
+  infoText: {
+    color: "white",
+    fontSize: 16,
+    textAlign: "center",
+    marginTop: 32,
   },
 });
