4ce4267e45865b20698edbbf9701ae70ee4d86c6
Severyn <severynson.kurach@gmail.com>
2023-01-14 06:23:30 +0200

    Section-10 DONE

---
+++ Section-10 (HTTP requests)/screens/RecentExpenses.js
@@ -0,0 +1,54 @@
+import {
+  useContext,
+  useEffect,
+  useState,
+  // useState
+} from "react";
+import { Alert } from "react-native";
+
+import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
+import LoadingOverlay from "../components/UI/LoadingOverlay";
+import { ExpensesContext } from "../store/expenses-context";
+import { getDateMinusDays } from "../util/date";
+import { fetchExpenses } from "../util/http";
+
+function RecentExpenses() {
+  const [isFetching, setIsFetching] = useState(true);
+  const expensesCtx = useContext(ExpensesContext);
+  // const [fetchedExpenses, setFetchedExpenses] = useState([]);
+
+  useEffect(() => {
+    async function getExpenses() {
+      setIsFetching(true);
+      try {
+        const expenses = await fetchExpenses();
+        expensesCtx.setExpense(expenses);
+      } catch (error) {
+        Alert.alert(error);
+      }
+      setIsFetching(false);
+      // setFetchedExpenses(expenses);
+    }
+
+    getExpenses();
+  }, []);
+
+  const recentExpenses = expensesCtx.expenses.filter((expense) => {
+    const today = new Date();
+    const date7DaysAgo = getDateMinusDays(today, 7);
+
+    return expense.date >= date7DaysAgo && expense.date <= today;
+  });
+
+  if (isFetching) return <LoadingOverlay />;
+
+  return (
+    <ExpensesOutput
+      expenses={recentExpenses}
+      expensesPeriod="Last 7 Days"
+      fallbackText="No expenses registered for the last 7 days."
+    />
+  );
+}
+
+export default RecentExpenses;
